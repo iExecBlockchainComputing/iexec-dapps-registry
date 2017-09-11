8c596beb6ef17e7a7322054747031638de4f2318
victor <victor@vbonhomme.fr>
2017-09-11 15:21:19 +0200

    add ropsten network with infura gateway

--- truffle.js
+++ truffle.js
@@ -4,6 +4,11 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    };
+    ropsten: {
+      host: "https://ropsten.infura.io/berv5GTB5cSdOJPPnqOq",
+      port: 8545,
+      network_id: "3" // Match any network id
+    };
   }
 };
