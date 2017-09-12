3c61643b78ee8c00f2f73244d065b74de96dd5ce
francois branciard <francois.branciard@iex.ec>
2017-09-13 01:44:09 +0200

    use default fonction iexecSubmit for helloworld

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -7,28 +7,4 @@ contract HelloWorld is IexecOracleAPI{
   }
 
 
-  function submitEcho(string helloText ){
-    iexecSubmit("echo",helloText);
-  }
-
-  function submitAndWaitEcho(string helloText ){
-    iexecSubmitAndWait("echo",helloText);
-  }
-
-  function statusHelloWorld(string workUid){
-    iexecStatus(workUid);
-  }
-
-  function resultHelloWorld(string workUid){
-    iexecResult(workUid);
-  }
-
-
-
-
-
-
-
-
-
 }
