bbf308a70015407d98dc8d92e1f7d3c8fe40daef
victor <victor@vbonhomme.fr>
2017-09-18 11:42:42 +0200

    add rinkeby testnet

--- truffle.js
+++ truffle.js
@@ -8,7 +8,14 @@ module.exports = {
         ropsten: {
             host: "https://ropsten.infura.io/berv5GTB5cSdOJPPnqOq",
             port: 8545,
-            network_id: "3" // Match any network id
+            network_id: "3"
+        },
+        rinkeby: {
+            host: "https://rinkeby.infura.io/berv5GTB5cSdOJPPnqOq",
+            port: 8545,
+            network_id: "4",
+            gasPriceMultiplier: 3,
+            gas: 4700000
         },
     }
 };
