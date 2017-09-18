bc907e84912ee9d8d26ff2316df335bb8e0bcb88
victor <victor@vbonhomme.fr>
2017-09-18 21:59:01 +0200

    added iexecOracleAddress for all known testnet

--- truffle.js
+++ truffle.js
@@ -8,21 +8,23 @@ module.exports = {
         ropsten: {
             host: "https://ropsten.infura.io/berv5GTB5cSdOJPPnqOq",
             port: 8545,
-            network_id: "3"
+            network_id: "3",
+            gasPriceMultiplier: 2,
+            iexecOracleAddress: "0x552075c9e40b050c8b61339b770e2a21e9014b3c",
         },
         rinkeby: {
             host: "https://rinkeby.infura.io/berv5GTB5cSdOJPPnqOq",
             port: 8545,
             network_id: "4",
-            gasPriceMultiplier: 3,
-            gas: 4700000
+            gasPriceMultiplier: 2,
+            iexecOracleAddress: "0x0b1ea4ff347e05ca175e3d3cfb4499bc4ad5ada5",
         },
         kovan: {
             host: "https://kovan.infura.io/berv5GTB5cSdOJPPnqOq",
             port: 8545,
             network_id: "42",
             gasPriceMultiplier: 2,
-            gas: 4700000,
+            iexecOracleAddress: "0xe6b658facf9621eff76a0d649c61dba4c8de85fb",
         },
     }
 };
