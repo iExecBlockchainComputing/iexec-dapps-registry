f84dbff314ee19db7d84f3700dbcb563da163dfb
victor <victor@vbonhomme.fr>
2017-09-19 00:37:02 +0200

    added constructor example for kovan and rinkeby testnet

--- iexec.js
+++ iexec.js
@@ -1,4 +1,6 @@
 module.exports = {
     name: 'Factorial',
     constructorArgs: ['0x552075c9e40b050c8b61339b770e2a21e9014b3c'],
+    // constructorArgs: ['0x0b1ea4ff347e05ca175e3d3cfb4499bc4ad5ada5'], // rinkeby
+    // constructorArgs: ['0xe6b658facf9621eff76a0d649c61dba4c8de85fb'], // kovan
 };
