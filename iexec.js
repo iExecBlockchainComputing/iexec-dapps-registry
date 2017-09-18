4a6ebe5f60490a0573a8413a878b4d46cea3e0cf
francois branciard <francois.branciard@iex.ec>
2017-09-18 18:57:19 +0200

    add kovan and rinkeby oracle address

--- iexec.js
+++ iexec.js
@@ -1,4 +1,7 @@
 module.exports = {
     name: 'Factorial',
     oracleAddress: '0x552075c9e40b050c8b61339b770e2a21e9014b3c',
+    // ropsten   oracleAddress: '0x552075c9e40b050c8b61339b770e2a21e9014b3c',
+    // kovan     oracleAddress: '0xe6b658facf9621eff76a0d649c61dba4c8de85fb',
+    // rinkeby   oracleAddress: '0x0b1ea4ff347e05ca175e3d3cfb4499bc4ad5ada5',
 };
