5a7d9c8ea63cdeb4774a8bd55f3966c32192af60
francois branciard <francois.branciard@iex.ec>
2017-09-19 11:09:59 +0000

    init project

---
+++ contracts/MyContract.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.11;
+import "./IexecOracleAPI.sol";
+contract MyContract is IexecOracleAPI{
+
+    function MyContract (address _iexecOracleAddress) IexecOracleAPI(_iexecOracleAddress){
+
+    }
+
+}
