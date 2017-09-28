ca65b3fe064816334da561532b275fcd83d06af4
francois branciard <francois.branciard@iex.ec>
2017-09-28 22:54:23 +0200

    debug stockfish

--- contracts/Stockfish.sol
+++ contracts/Stockfish.sol
@@ -1,6 +1,17 @@
 pragma solidity ^0.4.11;
 import "./IexecOracleAPI.sol";
+import "./strings.sol";
 contract Stockfish is IexecOracleAPI{
+
+    using strings for *;
+
+    event Logs(string status, address indexed user); // logs for the front-end or smart contract to react correctly
+
+    struct Chess {
+    string game;
+    }
+
+    mapping (address => Chess) chessRegister;
 
     function Stockfish (address _iexecOracleAddress) IexecOracleAPI(_iexecOracleAddress){
 
