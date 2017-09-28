f187d8ae82a2cb8b29f5f211f2d772177186c1ba
francois branciard <francois.branciard@iex.ec>
2017-09-28 22:58:53 +0200

    debug stockfish

--- contracts/Stockfish.sol
+++ contracts/Stockfish.sol
@@ -17,5 +17,56 @@ contract Stockfish is IexecOracleAPI{
 
     }
 
+    function checkValidity(string userMove) constant returns (bool validity) {
+        if (userMove.toSlice().len() != 4) return false;
+        else if (bytes(userMove)[0] < "a" || bytes(userMove)[0] > "h") return false;
+        else if (bytes(userMove)[1] < "1" || bytes(userMove)[1] > "8") return false;
+        else if (bytes(userMove)[2] < "a" || bytes(userMove)[2] > "h") return false;
+        else if (bytes(userMove)[3] < "1" || bytes(userMove)[3] > "8") return false;
+        else return true;
+    }
+
+    function setParam(string userMove)  {
+        bool validity = checkValidity(userMove);
+        if (!validity) {
+            Logs("ERROR", msg.sender);
+            throw;
+        }
+        if (chessRegister[msg.sender].game.toSlice().len() != 0)
+        chessRegister[msg.sender].game = " ".toSlice().concat(chessRegister[msg.sender].game.toSlice()); // separate moves
+        chessRegister[msg.sender].game = userMove.toSlice().concat(chessRegister[msg.sender].game.toSlice());
+        //iexecSubmit(string appName, string param)
+       // iexecSubmit("stockfish",chessRegister[msg.sender].game);
+    }
+
+    function undoMove() {
+        var game = chessRegister[msg.sender].game.toSlice();
+        uint8 nbmoves;
+        for( nbmoves=0; nbmoves<2; nbmoves++) { // delete the player move and the IA move
+            game.split(" ".toSlice());
+        }
+        chessRegister[msg.sender].game = game.toString();
+        Logs("Task finished !", msg.sender);
+    }
+
+    function flushGame() {
+        chessRegister[msg.sender].game = "";
+        Logs("Task finished !", msg.sender);
+    }
+
+
+
+    function pushResult(address userAddr, string newAIMove) internal {
+        if (chessRegister[userAddr].game.toSlice().len() != 0)
+        chessRegister[userAddr].game = " ".toSlice().concat(chessRegister[userAddr].game.toSlice());
+        chessRegister[userAddr].game = newAIMove.toSlice().concat(chessRegister[userAddr].game.toSlice());
+        Logs("Task finished !",userAddr);
+    }
+
+    function getResult() constant returns (string game) {
+        return (chessRegister[msg.sender].game);
+    }
+
+
 
 }
