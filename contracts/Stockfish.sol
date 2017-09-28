7eccda997f8d3220418bbf9627abf1f51a405aad
francois branciard <francois.branciard@iex.ec>
2017-09-28 13:44:48 +0200

    init stockfish

---
+++ contracts/Stockfish.sol
@@ -0,0 +1,75 @@
+pragma solidity ^0.4.11;
+import "./IexecOracleAPI.sol";
+import "./strings.sol";
+contract Stockfish is IexecOracleAPI{
+
+
+    using strings for *;
+
+    struct Chess {
+    string game;
+    }
+
+    mapping (address => Chess) chessRegister;
+
+    function Stockfish (address _iexecOracleAddress) IexecOracleAPI(_iexecOracleAddress){
+
+    }
+
+    function checkValidity(string userMove) constant returns (bool validity) {
+        if (userMove.toSlice().len() != 4) return false;
+        else if (bytes(userMove)[0] < "a" || bytes(userMove)[0] > "h") return false;
+        else if (bytes(userMove)[1] < "1" || bytes(userMove)[1] > "8") return false;
+        else if (bytes(userMove)[2] < "a" || bytes(userMove)[2] > "h") return false;
+        else if (bytes(userMove)[3] < "1" || bytes(userMove)[3] > "8") return false;
+        else return true;
+    }
+
+    function setParam(string userMove)  {
+        bool validity = checkValidity(userMove);
+        if (!validity) {
+            Launch("ERROR", msg.sender);
+            throw;
+        }
+        if (chessRegister[msg.sender].game.toSlice().len() != 0)
+        chessRegister[msg.sender].game = " ".toSlice().concat(chessRegister[msg.sender].game.toSlice()); // separate moves
+        chessRegister[msg.sender].game = userMove.toSlice().concat(chessRegister[msg.sender].game.toSlice());
+        //iexecSubmit(string appName, string param)
+        iexecSubmit("stockfish",chessRegister[msg.sender].game);
+    }
+
+    function undoMove() {
+        var game = chessRegister[msg.sender].game.toSlice();
+        for(var nbmoves=0; nbmoves<2; nbmoves++) { // delete the player move and the IA move
+            game.split(" ".toSlice());
+        }
+        chessRegister[msg.sender].game = game.toString();
+        Logs("Task finished !", msg.sender);
+    }
+
+    function flushGame() {
+        chessRegister[msg.sender].game = "";
+        Logs("Task finished !", msg.sender);
+    }
+
+    //overwrite IexecOracleAPI iexecSubmitCallback
+    function iexecSubmitCallback(bytes32 submitTxHash, address user, string appName, string stdout) returns (bool){
+        require(msg.sender == iexecOracleAddress);
+        IexecSubmitCallback(submitTxHash,user,appName,stdout);
+        pushResult(user,stdout);
+        return true;
+    }
+
+    function pushResult(address userAddr, string newAIMove) internal {
+        if (chessRegister[userAddr].game.toSlice().len() != 0)
+        chessRegister[userAddr].game = " ".toSlice().concat(chessRegister[userAddr].game.toSlice());
+        chessRegister[userAddr].game = newAIMove.toSlice().concat(chessRegister[userAddr].game.toSlice());
+        Logs("Task finished !",userAddr);
+    }
+
+    function getResult() constant returns (string game) {
+        return (chessRegister[msg.sender].game);
+    }
+
+
+}
--- iexec.js
+++ iexec.js
@@ -1,6 +1,7 @@
 module.exports = {
-    name: 'MyContract',
-    constructorArgs: ['0x552075c9e40b050c8b61339b770e2a21e9014b3c'],
-    // constructorArgs: ['0x0b1ea4ff347e05ca175e3d3cfb4499bc4ad5ada5'], // rinkeby
-    // constructorArgs: ['0xe6b658facf9621eff76a0d649c61dba4c8de85fb'], // kovan
+    name: 'Stockfish',
+    constructorArgs: ['0x552075c9e40b050c8b61339b770e2a21e9014b3c'], // bridge sprint hello world ropsten
+    // constructorArgs: ['0x0b1ea4ff347e05ca175e3d3cfb4499bc4ad5ada5'], // bridge sprint hello world rinkeby
+    // constructorArgs: ['0xe6b658facf9621eff76a0d649c61dba4c8de85fb'], // bridge sprint hello world kovan
+
 };
