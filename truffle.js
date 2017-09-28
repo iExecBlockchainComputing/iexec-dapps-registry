d543e379c89f9a7683976c67a324ee814e31989f
francois branciard <francois.branciard@iex.ec>
2017-09-28 17:44:45 +0200

    init stockfish

--- truffle.js
+++ truffle.js
@@ -1,7 +1,7 @@
 module.exports = {
     networks: {
         development: {
-            host: "localhost",
+            host: "http://localhost",
             port: 8545,
             network_id: "*" // Match any network id
         },
