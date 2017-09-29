e44c28ca3d37a11beb25b03d71344c465ec13096
francois branciard <francois.branciard@iex.ec>
2017-09-29 15:34:16 +0200

    target  oracle v0.1.2 ropsten rinkeby kovan

--- truffle.js
+++ truffle.js
@@ -10,7 +10,7 @@ module.exports = {
             host: "https://ropsten.infura.io/berv5GTB5cSdOJPPnqOq",
             port: 8545,
             network_id: "3",
-            iexecOracleAddress: "0xb34406538112bd2b3036b2c417c7cff827777a11",//oracle v0.1.1
+            iexecOracleAddress: "0xb34406538112bd2b3036b2c417c7cff827777a11",//oracle v0.1.2
             gasLimitMultiplier: 5
 
             // gasPriceMultiplier: 2,  // use factor 2 of the network estimated gasPrice
@@ -22,14 +22,14 @@ module.exports = {
             host: "https://rinkeby.infura.io/berv5GTB5cSdOJPPnqOq",
             port: 8545,
             network_id: "4",
-            iexecOracleAddress: "0x98275d4b6511ef05ed063d127dd82b72588326c9",//oracle v0.1.1
+            iexecOracleAddress: "0x98275d4b6511ef05ed063d127dd82b72588326c9",//oracle v0.1.2
             gasLimitMultiplier: 5
         },
         kovan: {
             host: "https://kovan.infura.io/berv5GTB5cSdOJPPnqOq",
             port: 8545,
             network_id: "42",
-            iexecOracleAddress: "0xb81d38d843cb526a3d0c3130d568fe09799135aa",//oracle v0.1.1
+            iexecOracleAddress: "0xb81d38d843cb526a3d0c3130d568fe09799135aa",//oracle v0.1.2
             gasLimitMultiplier: 5
         },
     }
