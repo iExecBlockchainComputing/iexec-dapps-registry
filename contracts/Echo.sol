9049e3022830b29d14392c1e81a7681aed107f2c
francois branciard <francois.branciard@iex.ec>
2017-10-04 10:26:50 +0200

    add dependency of iexec-oracle-contract with npm

--- contracts/Echo.sol
+++ contracts/Echo.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.4.11;
-import "./IexecOracleAPI.sol";
+import "iexec-oracle-contract/contracts/IexecOracleAPI.sol";
 contract Echo is IexecOracleAPI{
 
   function Echo (address _iexecOracleAddress) IexecOracleAPI(_iexecOracleAddress){
