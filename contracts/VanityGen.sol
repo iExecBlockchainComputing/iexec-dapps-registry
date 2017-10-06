49ded9848b66cd0ddca6a0a45798e8bf510e1ced
francois branciard <francois.branciard@iex.ec>
2017-10-06 12:14:37 +0200

    init vanitygen

---
+++ contracts/VanityGen.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.11;
+import "./IexecOracleAPI.sol";
+contract VanityGen is IexecOracleAPI{
+
+    function VanityGen (address _iexecOracleAddress) IexecOracleAPI(_iexecOracleAddress,0ie){
+
+    }
+
+    function generateVanity(string pattern,string publickey) payable external {
+        require (bytes(pattern).length > 0);
+        require (bytes(pattern).length <= 5);// max vanity lenght
+        require(iexecSubmit("vanitygen","-P "+pattern+ " 1" + publickey ));
+    }
+
+}
+
+
