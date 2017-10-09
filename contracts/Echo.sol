5058f4f5f93cd67e79e854d64360135bd8f35f43
francois branciard <francois.branciard@iex.ec>
2017-10-10 00:33:44 +0200

    update to oracle v0.1.3 with RLC payment and gas callback price

--- contracts/Echo.sol
+++ contracts/Echo.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.11;
 import "iexec-oracle-contract/contracts/IexecOracleAPI.sol";
 contract Echo is IexecOracleAPI{
 
-  function Echo (address _iexecOracleAddress) IexecOracleAPI(_iexecOracleAddress){
+  function Echo (address _iexecOracleAddress) IexecOracleAPI(_iexecOracleAddress,0){
 
   }
 
--- iexec.js
+++ iexec.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: 'Echo',
-  constructorArgs: ['0xb34406538112bd2b3036b2c417c7cff827777a11'], // oracle v0.1.2 ropsten
-  // constructorArgs: ['0x98275d4b6511ef05ed063d127dd82b72588326c9'], // oracle v0.1.2 rinkeby
-  // constructorArgs: ['0xb81d38d843cb526a3d0c3130d568fe09799135aa'], // oracle v0.1.2 kovan
+  constructorArgs: ['0x7314dc4d7794b5e7894212ca1556ae8e3de58621'], // oracle v0.1.3 ropsten
+  // constructorArgs: ['0xf1e6ad3a7ef0c86c915f0fedf80ed851809bea90'], // oracle v0.1.3 rinkeby
+  // constructorArgs: ['0xc57538846ec405ea25deb00e0f9b29a432d53507'], // oracle v0.1.3 kovan
 };
