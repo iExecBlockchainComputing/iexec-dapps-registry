a6a3d9f1d417cc2d61d2df3d289f3181c924e44a
francois branciard <francois.branciard@iex.ec>
2017-10-11 11:45:15 +0200

    target to     "iexec-oracle-contract": "github:iExecBlockchainComputing/iexec-oracle-contract#v1.0.3"

--- iexec.js
+++ iexec.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: 'Echo',
-  constructorArgs: ['0x00b24fcfaa2d0c5a69887c8b1cefb937bdee84f6'], // oracle v0.1.3 ropsten
-  // constructorArgs: ['0xb34406538112bd2b3036b2c417c7cff827777a11'], // oracle v0.1.3 rinkeby
-  // constructorArgs: ['0x56ba3ef8e60c4f8e86c99225e5dade84578a178e'], // oracle v0.1.3 kovan
+  constructorArgs: ['0x00b24fcfaa2d0c5a69887c8b1cefb937bdee84f6'], // oracle v1.0.3 ropsten
+  // constructorArgs: ['0xb34406538112bd2b3036b2c417c7cff827777a11'], // oracle v1.0.3 rinkeby
+  // constructorArgs: ['0x56ba3ef8e60c4f8e86c99225e5dade84578a178e'], // oracle v1.0.3 kovan
 };
--- truffle.js
+++ truffle.js
@@ -9,7 +9,7 @@ module.exports = {
             host: "https://ropsten.infura.io/berv5GTB5cSdOJPPnqOq",
             port: 8545,
             network_id: "3",
-            iexecOracleAddress: "0x00b24fcfaa2d0c5a69887c8b1cefb937bdee84f6",//oracle v0.1.3
+            iexecOracleAddress: "0x00b24fcfaa2d0c5a69887c8b1cefb937bdee84f6",//oracle v1.0.3
             // gasPriceMultiplier: 2,  // use factor 2 of the network estimated gasPrice
             // gasLimitMultiplier: 4,  // use factor 4 of the network estimated gasLimit
             // gasPrice: 21000000000  // manually set the gasPrice in gwei. Prefer "gasPriceMultiplier"
@@ -19,13 +19,13 @@ module.exports = {
             host: "https://rinkeby.infura.io/berv5GTB5cSdOJPPnqOq",
             port: 8545,
             network_id: "4",
-            iexecOracleAddress: "0xb34406538112bd2b3036b2c417c7cff827777a11",//oracle v0.1.3
+            iexecOracleAddress: "0xb34406538112bd2b3036b2c417c7cff827777a11",//oracle v1.0.3
         },
         kovan: {
             host: "https://kovan.infura.io/berv5GTB5cSdOJPPnqOq",
             port: 8545,
             network_id: "42",
-            iexecOracleAddress: "0x56ba3ef8e60c4f8e86c99225e5dade84578a178e",//oracle v0.1.3
+            iexecOracleAddress: "0x56ba3ef8e60c4f8e86c99225e5dade84578a178e",//oracle v1.0.3
         },
     }
 };
