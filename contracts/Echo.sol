a6861c97df0fdf98c88c559dca29f8e324e1fe16
francois branciard <francois.branciard@iex.ec>
2017-10-11 17:29:51 +0200

    iexec-oracle 1.0.5

--- contracts/Echo.sol
+++ contracts/Echo.sol
@@ -2,7 +2,9 @@ pragma solidity ^0.4.11;
 import "iexec-oracle-contract/contracts/IexecOracleAPI.sol";
 contract Echo is IexecOracleAPI{
 
-  function Echo (address _iexecOracleAddress) IexecOracleAPI(_iexecOracleAddress,0){
+  uint public constant DAPP_PRICE = 0;
+
+  function Echo (address _iexecOracleAddress) IexecOracleAPI(_iexecOracleAddress,DAPP_PRICE){
 
   }
 
--- truffle.js
+++ truffle.js
@@ -9,7 +9,6 @@ module.exports = {
             host: "https://ropsten.infura.io/berv5GTB5cSdOJPPnqOq",
             port: 8545,
             network_id: "3",
-            iexecOracleAddress: "0x065097b71b2e372eee4e31374b0262fa08238754",//oracle v1.0.4
             // gasPriceMultiplier: 2,  // use factor 2 of the network estimated gasPrice
             // gasLimitMultiplier: 4,  // use factor 4 of the network estimated gasLimit
             // gasPrice: 21000000000  // manually set the gasPrice in gwei. Prefer "gasPriceMultiplier"
@@ -19,13 +18,11 @@ module.exports = {
             host: "https://rinkeby.infura.io/berv5GTB5cSdOJPPnqOq",
             port: 8545,
             network_id: "4",
-            iexecOracleAddress: "0x9752f4bb6ab182a98e7347936e2172e7c0821338",//oracle v1.0.4
         },
         kovan: {
             host: "https://kovan.infura.io/berv5GTB5cSdOJPPnqOq",
             port: 8545,
             network_id: "42",
-            iexecOracleAddress: "0x77040475d5cf05e9dd44f96d7dab3b7da7adbc6a",//oracle v1.0.4
         },
     }
 };
