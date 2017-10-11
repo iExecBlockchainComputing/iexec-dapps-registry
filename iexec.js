e0d75cea05455c4a2ca820bdadf52909fa0d6e30
francois branciard <francois.branciard@iex.ec>
2017-10-11 15:19:40 +0200

    iexec-oracle 1.0.4

--- iexec.js
+++ iexec.js
@@ -1,6 +1,12 @@
+const oracleJSON = require('iexec-oracle-contract');
+
+const ROPSTEN_ORACLE_ADDRESS=oracleJSON.networks['3'].address;
+const RINKEBY_ORACLE_ADDRESS=oracleJSON.networks['4'].address;
+const KOVAN_ORACLE_ADDRESS=oracleJSON.networks['42'].address;
+
 module.exports = {
   name: 'Echo',
-  constructorArgs: ['0x065097b71b2e372eee4e31374b0262fa08238754'], // oracle v1.0.4 ropsten
-  // constructorArgs: ['0x9752f4bb6ab182a98e7347936e2172e7c0821338'], // oracle v1.0.4 rinkeby
-  // constructorArgs: ['0x77040475d5cf05e9dd44f96d7dab3b7da7adbc6a'], // oracle v1.0.4 kovan
+  constructorArgs: [ROPSTEN_ORACLE_ADDRESS],
+  // constructorArgs: [RINKEBY_ORACLE_ADDRESS],
+  // constructorArgs: [KOVAN_ORACLE_ADDRESS],
 };
