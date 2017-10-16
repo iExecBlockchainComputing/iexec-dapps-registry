d886107958cd8e0a4358397db5276a322efb709e
victor <victor@vbonhomme.fr>
2017-10-16 11:22:16 +0200

    remove constructorArgs from iexec.js

--- iexec.js
+++ iexec.js
@@ -1,12 +1,3 @@
-const oracleJSON = require('iexec-oracle-contract/build/contracts/IexecOracle.json');
-
-const ROPSTEN_ORACLE_ADDRESS = oracleJSON.networks['3'].address;
-const RINKEBY_ORACLE_ADDRESS = oracleJSON.networks['4'].address;
-const KOVAN_ORACLE_ADDRESS = oracleJSON.networks['42'].address;
-
 module.exports = {
   name: 'Echo',
-  constructorArgs: [ROPSTEN_ORACLE_ADDRESS],
-  // constructorArgs: [RINKEBY_ORACLE_ADDRESS],
-  // constructorArgs: [KOVAN_ORACLE_ADDRESS],
 };
