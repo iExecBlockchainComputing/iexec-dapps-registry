b0eea6a19d288166f9d2d540db9f3432a5f2cace
victor <victor@vbonhomme.fr>
2017-10-16 11:18:55 +0200

    move constructor to truffle.js

--- truffle.js
+++ truffle.js
@@ -1,3 +1,9 @@
+const oracleJSON = require('iexec-oracle-contract/build/contracts/IexecOracle.json');
+
+const ROPSTEN_ORACLE_ADDRESS = oracleJSON.networks['3'].address;
+const RINKEBY_ORACLE_ADDRESS = oracleJSON.networks['4'].address;
+const KOVAN_ORACLE_ADDRESS = oracleJSON.networks['42'].address;
+
 module.exports = {
     networks: {
         development: {
@@ -9,22 +15,25 @@ module.exports = {
             host: "https://ropsten.infura.io/berv5GTB5cSdOJPPnqOq",
             port: 8545,
             network_id: "3",
+            constructorArgs: [ROPSTEN_ORACLE_ADDRESS],
             // gasPriceMultiplier: 2,  // use factor 2 of the network estimated gasPrice
             // gasLimitMultiplier: 4,  // use factor 4 of the network estimated gasLimit
-            // gasPrice: 21000000000  // manually set the gasPrice in gwei. Prefer "gasPriceMultiplier"
-             gas: 400000  // manually set the gas limit in gwei. Prefer "gasLimitMultiplier"
+            // gasPrice: 21000000000,  // manually set the gasPrice in gwei. Prefer "gasPriceMultiplier"
+            // gas: 400000,  // manually set the gas limit in gwei. Prefer "gasLimitMultiplier"
         },
         rinkeby: {
             host: "https://rinkeby.infura.io/berv5GTB5cSdOJPPnqOq",
             port: 8545,
             network_id: "4",
-            gas: 400000  // manually set the gas limit in gwei. Prefer "gasLimitMultiplier"
+            constructorArgs: [RINKEBY_ORACLE_ADDRESS],
+            // gas: 400000,  // manually set the gas limit in gwei. Prefer "gasLimitMultiplier"
         },
         kovan: {
             host: "https://kovan.infura.io/berv5GTB5cSdOJPPnqOq",
             port: 8545,
             network_id: "42",
-            gas: 400000  // manually set the gas limit in gwei. Prefer "gasLimitMultiplier"
+            constructorArgs: [KOVAN_ORACLE_ADDRESS],
+            // gas: 400000,  // manually set the gas limit in gwei. Prefer "gasLimitMultiplier"
         },
     }
 };
