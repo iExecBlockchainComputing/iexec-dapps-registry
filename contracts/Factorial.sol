4223ac76b014285ad758840dcdef1d828edfaab7
fbranciard <francois.branciard@iex.ec>
2017-10-17 12:28:23 +0200

    add DAPP_PRICE

--- contracts/Factorial.sol
+++ contracts/Factorial.sol
@@ -2,7 +2,9 @@ pragma solidity ^0.4.11;
 import "iexec-oracle-contract/contracts/IexecOracleAPI.sol";
 contract Factorial is IexecOracleAPI{
 
-  function Factorial (address _iexecOracleAddress) IexecOracleAPI(_iexecOracleAddress){
+  uint public constant DAPP_PRICE = 1;
+
+  function Factorial (address _iexecOracleAddress) IexecOracleAPI(_iexecOracleAddress,DAPP_PRICE){
 
   }
 
