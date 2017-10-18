81d7046b79335aee56ecb25a12da7f6b0a24c7c3
fbranciard <francois.branciard@iex.ec>
2017-10-18 11:55:15 +0200

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -1,20 +1,23 @@
+const oracleJSON = require('iexec-oracle-contract/build/contracts/IexecOracle.json');
+
+const ROPSTEN_ORACLE_ADDRESS = oracleJSON.networks['3'].address;
+const RINKEBY_ORACLE_ADDRESS = oracleJSON.networks['4'].address;
+const KOVAN_ORACLE_ADDRESS = oracleJSON.networks['42'].address;
+
 module.exports = {
     networks: {
         development: {
             host: "http://localhost:8545",
             port: 8545,
-            network_id: "42",
-            gasLimitMultiplier: 5
+            network_id: "*" // Match any network id
         },
         ropsten: {
             host: "https://ropsten.infura.io/berv5GTB5cSdOJPPnqOq",
             port: 8545,
             network_id: "3",
-            iexecOracleAddress: "0xb34406538112bd2b3036b2c417c7cff827777a11",//oracle v0.1.2
-            gasLimitMultiplier: 5
-
+            constructorArgs: [ROPSTEN_ORACLE_ADDRESS],
             // gasPriceMultiplier: 2,  // use factor 2 of the network estimated gasPrice
-            // gasLimitMultiplier: 4,  // use factor 4 of the network estimated gasLimit
+             gasLimitMultiplier: 5,  // use factor 2 of the network estimated gasLimit
             // gasPrice: 21000000000  // manually set the gasPrice in gwei. Prefer "gasPriceMultiplier"
             // gas: 400000  // manually set the gas limit in gwei. Prefer "gasLimitMultiplier"
         },
@@ -22,15 +25,15 @@ module.exports = {
             host: "https://rinkeby.infura.io/berv5GTB5cSdOJPPnqOq",
             port: 8545,
             network_id: "4",
-            iexecOracleAddress: "0x98275d4b6511ef05ed063d127dd82b72588326c9",//oracle v0.1.2
-            gasLimitMultiplier: 5
+            gasLimitMultiplier: 5, 
+            constructorArgs: [RINKEBY_ORACLE_ADDRESS],
         },
         kovan: {
             host: "https://kovan.infura.io/berv5GTB5cSdOJPPnqOq",
             port: 8545,
             network_id: "42",
-            iexecOracleAddress: "0xb81d38d843cb526a3d0c3130d568fe09799135aa",//oracle v0.1.2
-            gasLimitMultiplier: 5
+            gasLimitMultiplier: 5, 
+            constructorArgs: [KOVAN_ORACLE_ADDRESS],
         },
     }
 };
