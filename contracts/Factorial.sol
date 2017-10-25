8f2b0c68d6dfaa9fccd446803c6d1473cfbbde90
fbranciard <francois.branciard@iex.ec>
2017-10-25 18:00:32 +0200

    Update Factorial.sol

--- contracts/Factorial.sol
+++ contracts/Factorial.sol
@@ -3,8 +3,9 @@ import "iexec-oracle-contract/contracts/IexecOracleAPI.sol";
 contract Factorial is IexecOracleAPI{
 
   uint public constant DAPP_PRICE = 1;
+  string public constant DAPP_NAME = "factorial";
 
-  function Factorial (address _iexecOracleAddress) IexecOracleAPI(_iexecOracleAddress,DAPP_PRICE){
+  function Factorial (address _iexecOracleAddress) IexecOracleAPI(_iexecOracleAddress,DAPP_PRICE,DAPP_NAME){
 
   }
 
