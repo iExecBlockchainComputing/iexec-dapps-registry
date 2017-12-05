557e6075d6b320c71db096e0843cb7361c46506e
jeremyjams <jeremy.toussaint@iex.ec>
2017-12-05 15:46:34 +0100

    Changed contract

--- contracts/Ffmpeg.sol
+++ contracts/Ffmpeg.sol
@@ -3,7 +3,7 @@ import "iexec-oracle-contract/contracts/IexecOracleAPI.sol";
 contract Ffmpeg is IexecOracleAPI{
 
     uint public constant DAPP_PRICE = 1;
-    string public constant DAPP_NAME = "ffmpeg";
+    string public constant DAPP_NAME = "Ffmpeg";
     
     function Ffmpeg (address _iexecOracleAddress) IexecOracleAPI(_iexecOracleAddress,DAPP_PRICE,DAPP_NAME){
 
