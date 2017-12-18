8b6654c98dbf2e59e8874d92d4b98387343c573d
victor <victor@vbonhomme.fr>
2017-12-18 13:29:59 +0100

    add iexec server (xw) address

--- truffle.js
+++ truffle.js
@@ -9,13 +9,15 @@ module.exports = {
         development: {
             host: "http://localhost:8545",
             port: 8545,
-            network_id: "*" // Match any network id
+            network_id: "*", // Match any network id
+            server: "https://localhost:443",
         },
         ropsten: {
             host: "https://ropsten.infura.io/berv5GTB5cSdOJPPnqOq",
             port: 8545,
             network_id: "3",
             constructorArgs: [ROPSTEN_ORACLE_ADDRESS],
+            server: "https://testxw.iex.ec:443",
             // gasPriceMultiplier: 2,  // use factor 2 of the network estimated gasPrice
             // gasLimitMultiplier: 4,  // use factor 4 of the network estimated gasLimit
             // gasPrice: 21000000000,  // manually set the gasPrice in gwei. Prefer "gasPriceMultiplier"
@@ -26,6 +28,7 @@ module.exports = {
             port: 8545,
             network_id: "4",
             constructorArgs: [RINKEBY_ORACLE_ADDRESS],
+            server: "https://testxw.iex.ec:443",
             // gas: 400000,  // manually set the gas limit in gwei. Prefer "gasLimitMultiplier"
         },
         kovan: {
@@ -33,6 +36,7 @@ module.exports = {
             port: 8545,
             network_id: "42",
             constructorArgs: [KOVAN_ORACLE_ADDRESS],
+            server: "https://testxw.iex.ec:443",
             // gas: 400000,  // manually set the gas limit in gwei. Prefer "gasLimitMultiplier"
         },
     }
