ddc6182073d3e2576f453b62eb65e08b4952a0bc
victor <victor@vbonhomme.fr>
2018-01-16 16:51:59 +0100

    iexec config for docker

--- iexec.js
+++ iexec.js
@@ -1,11 +1,16 @@
 module.exports = {
-  name: 'MyContract',
+  name: 'Docker',
   data: {
     type: 'BINARY',
     cpu: 'AMD64',
     os: 'LINUX',
   },
+  app: {
+    type: 'DOCKER',
+    launchscriptshuri: 'xw://devxw.iex.ec/f798a440-97f9-4660-8178-02955eec5c73',
+    unloadscriptshuri: 'xw://devxw.iex.ec/b535b4fd-fb7d-4e72-9c2b-345c6eb1b55f',
+  },
   work: {
-    cmdline: '10',
+    cmdline: 'hello-world',
   }
 };
