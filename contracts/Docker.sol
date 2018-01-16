543046643b1d9fcf35eb08e20ec2e985b85eed0e
victor <victor@vbonhomme.fr>
2018-01-17 00:42:26 +0100

    rename contract

--- contracts/Docker.sol
+++ contracts/Docker.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.4.11;
 import "iexec-oracle-contract/contracts/IexecOracleAPI.sol";
-contract MyContract is IexecOracleAPI{
+contract Docker is IexecOracleAPI{
 
     uint public constant DAPP_PRICE = 0;
     string public constant DAPP_NAME = "docker";
 
-    function MyContract (address _iexecOracleAddress) IexecOracleAPI(_iexecOracleAddress,DAPP_PRICE,DAPP_NAME){
+    function Docker (address _iexecOracleAddress) IexecOracleAPI(_iexecOracleAddress,DAPP_PRICE,DAPP_NAME){
 
     }
 
