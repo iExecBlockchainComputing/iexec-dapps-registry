f170e0b340cece437eaee6f715ae07df4cc173cf
victor <victor@vbonhomme.fr>
2018-01-25 11:29:21 +0100

    add blender sample dapp

--- iexec.js
+++ iexec.js
@@ -1,16 +1,11 @@
 module.exports = {
-  name: 'Docker',
-  data: {
-    type: 'BINARY',
-    cpu: 'AMD64',
-    os: 'LINUX',
-  },
+  name: 'Blender',
   app: {
     type: 'DOCKER',
-    launchscriptshuri: 'xw://devxw.iex.ec/f798a440-97f9-4660-8178-02955eec5c73',
-    unloadscriptshuri: 'xw://devxw.iex.ec/b535b4fd-fb7d-4e72-9c2b-345c6eb1b55f',
+    envvars: 'XWDOCKERIMAGE=ikester/blender',
   },
   work: {
-    cmdline: 'hello-world',
+    cmdline: '/host/buggy2.1.blend -o /host/frame_### -f 1',
+    dirinuri:'https://github.com/sulliwane/files/raw/master/buggy2.1.blend',
   }
 };
