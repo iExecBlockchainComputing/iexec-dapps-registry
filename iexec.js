408986c24ea79cc2bb0bbac4f04a3230aeac463b
francois branciard <francois.branciard@iex.ec>
2018-02-10 23:35:05 +0000

    init r-clifford-attractors

--- iexec.js
+++ iexec.js
@@ -1,11 +1,11 @@
 module.exports = {
-  name: 'RWordcloud',
+  name: 'CliffordAttractors',
   app: {
     type: 'DOCKER',
-    envvars: 'XWDOCKERIMAGE=iexechub/r-wordcloud',
+    envvars: 'XWDOCKERIMAGE=iexechub/r-clifford-attractors',
   },
   work: {
-      cmdline: 'Rscript iExecWordcloud.R https://iex.ec/app/uploads/2017/04/iExec-WPv2.0-English.pdf',
-      dirinuri: 'https://github.com/iExecBlockchainComputing/iexec-dapp-samples/raw/r-wordcloud/apps/iExecWordcloud.R',
+      cmdline: 'Rscript CliffordAttractors.R -1.24458046630025 -1.25191834103316 -1.81590817030519 -1.90866735205054',
+      dirinuri: 'https://github.com/iExecBlockchainComputing/iexec-dapp-samples/raw/r-clifford-attractors/apps/CliffordAttractors.R',
   }
 };
