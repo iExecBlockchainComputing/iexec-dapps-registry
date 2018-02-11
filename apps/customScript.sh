09a1edd83eff9314437e327ca091d0a1b601b549
francois branciard <francois.branciard@iex.ec>
2018-02-11 17:46:27 +0000

    test with file in the image

--- apps/customScript.sh
+++ apps/customScript.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-echo "---------------------------------"
+echo "---------------------------------\n"
 echo "This is my customScript in docker"
-echo "---------------------------------"
+echo "---------------------------------\n"
 echo "i cat a file. see dirinuri of iexec.js"
 cat text.txt
-echo "---------------------------------"
+echo "---------------------------------\n"
 echo "just echo your param you give me in cmdline of iexec.js"
 echo $1
-echo "---------------------------------"
+echo "---------------------------------\n"
+echo "i cat a file present in the docker image"
+cat MyFileInTheImage.txt
+echo "---------------------------------\n"
