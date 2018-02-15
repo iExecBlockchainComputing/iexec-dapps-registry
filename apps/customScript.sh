bdaac729017c49323250e6344c206d19db858f1c
francois branciard <francois.branciard@iex.ec>
2018-02-15 16:25:32 +0000

    WIP

--- apps/customScript.sh
+++ apps/customScript.sh
@@ -4,8 +4,6 @@ echo "---------------i cat a file. see dirinuri of iexec.js------------------\n"
 cat text.txt
 echo "--------------just echo your param you give me in cmdline of iexec.js-------------------\n"
 echo $1
-echo "-------------cat MyFile.txt--------------------\n"
-cat MyFile.txt
 echo "-------------cat /iExec/MyFileIniExecDir.txt --------------------\n"
 cat /iExec/MyFileIniExecDir.txt
 
