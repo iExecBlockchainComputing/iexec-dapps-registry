8c5291d6717bed021a1abd464c7ac81ca9c47ad8
victor <victor@vbonhomme.fr>
2018-03-02 16:17:28 +0100

    fix contract

--- contracts/VanityGen.sol
+++ contracts/VanityGen.sol
@@ -8,13 +8,6 @@ contract VanityGen is IexecOracleAPI{
     function VanityGen (address _iexecOracleAddress) IexecOracleAPI(_iexecOracleAddress,DAPP_PRICE,DAPP_NAME){
 
     }
-
-    function generateVanity(string pattern,string publickey) payable external {
-        require (bytes(pattern).length > 0);
-        require (bytes(pattern).length <= 5);// max vanity lenght
-        require(iexecSubmit("vanitygen","-P "+pattern+ " 1" + publickey ));
-    }
-
 }
 
 
