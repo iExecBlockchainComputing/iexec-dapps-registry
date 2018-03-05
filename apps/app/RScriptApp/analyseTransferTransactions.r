fda48716d65d36fb53fef9d3c762c24713d724e2
francois branciard <francois.branciard@iex.ec>
2018-03-05 11:45:19 +0100

    change analyseTransferTransactions.r draw x = blocknumber,  y= value transfer

--- apps/app/RScriptApp/analyseTransferTransactions.r
+++ apps/app/RScriptApp/analyseTransferTransactions.r
@@ -17,25 +17,10 @@ transferTransactionsJson_flat <- flatten(transferTransactions_json)
 transferTransactions_tbl <- as_data_frame(transferTransactionsJson_flat)
 #transferTransactions_tbl
 
-#------------------------
-# analyse data : group_by transfer value
-#------------------------
-transferTransactionsAnalyseData <- transferTransactions_tbl %>% group_by(value) %>%
-summarise(
-   n = n()
- ) %>%
-arrange(desc(value))
-#transferTransactionsAnalyseData
-
-#---------------------------------------------------------------
-# write analyse result in a file transferTransactionsAnalyse.csv
-#---------------------------------------------------------------
-write.csv2(transferTransactionsAnalyseData,file = "/host/transferTransactionsAnalyse.csv")
-
 #--------------------------------------------------------
-# create histogram image transferTransactionsAnalyse.png
+# create plot points
 #--------------------------------------------------------
-p <- ggplot(data=transferTransactionsAnalyseData, aes(x=value,y=n, fill=n))
-p <- p + geom_histogram(stat = "identity")
-p <- p + ggtitle("ERC20 value transfer") + xlab("value transfer") + ylab("count")
+p <- ggplot(data=transferTransactions_tbl, aes(x=blockNumber,y=value))
+p <-p + geom_point(alpha = 1/100)
+p <- p + ggtitle("ERC20 Transfer value") + xlab("blockNumber") + ylab("Transfer value")
 ggsave(plot=p, file="/host/transferTransactionsAnalyse.png")
