faf9924dc535270f510508e93b6e1b27984daacf
francois branciard <francois.branciard@iex.ec>
2018-03-05 10:35:18 +0100

    update iexechub/tta:step3 to iexechub/tta:latest

--- iexec.js
+++ iexec.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'TTA',
   app: {
     type: 'DOCKER',
-    envvars: 'XWDOCKERIMAGE=iexechub/tta:step3',
+    envvars: 'XWDOCKERIMAGE=iexechub/tta:latest',
   },
   work: {
     cmdline: 'bash /app/app.sh 0x7314dc4d7794b5e7894212ca1556ae8e3de58621 100 http://54.91.54.77:8545',
