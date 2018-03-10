c135541343f0c250e811a7044fbc7b324c8ca4b1
victor <victor@vbonhomme.fr>
2018-03-10 12:21:50 +0100

    update sample work

--- iexec.js
+++ iexec.js
@@ -5,7 +5,6 @@ module.exports = {
     envvars: 'XWDOCKERIMAGE=bigpapoo/sox',
   },
   work: {
-    cmdline: `mysox sound.mp3 -r 8000 -c2 sound.wav`,
-    dirinuri: 'https://github.com/iExecBlockchainComputing/iexec-dapp-samples/raw/gimp/logo.png',
+    cmdline: `mysox --help`,
   }
 };
