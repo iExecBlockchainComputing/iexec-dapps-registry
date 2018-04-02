34b913e991d5bf4af565fedd9383446e3e3d8f32
francois branciard <francois.branciard@iex.ec>
2018-04-02 16:22:13 +0200

    exemple zip dirinuri OK

--- apps/customScript.sh
+++ apps/customScript.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "-------------This is my customScript in docker--------------------\n"
-echo "---------------i cat a file. see dirinuri of iexec.js------------------\n"
+echo "---------------i cat a file present in the dirinuri zip. see dirinuri line of iexec.js------------------\n"
 cat text.txt
+echo "---------------find image present in the dirinuri zip .see dirinuri line of iexec.js------------------\n"
+ls *.png
 echo "--------------just echo your param you give me in cmdline of iexec.js-------------------\n"
 echo $1
 echo "-------------cat /iExec/MyFileIniExecDir.txt --------------------\n"
