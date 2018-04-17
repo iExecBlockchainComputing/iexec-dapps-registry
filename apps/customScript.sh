3924b88235125d8e6105bccbcd392fcb875753cc
francois branciard <francois.branciard@iex.ec>
2018-04-17 11:53:34 +0200

    update test for https://github.com/iExecBlockchainComputing/iexec-sdk/issues/33

--- apps/customScript.sh
+++ apps/customScript.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 echo "-------------This is my customScript in docker--------------------\n"
 echo "---------------i cat a file present in the dirinuri zip. see dirinuri line of iexec.js------------------\n"
-cat text.txt
+cat /iexec/text.txt
 echo "---------------find image present in the dirinuri zip .see dirinuri line of iexec.js------------------\n"
-ls *.png
+ls /iexec/*.png
 echo "--------------just echo your param you give me in cmdline of iexec.js-------------------\n"
 echo $1
-echo "-------------cat /iExec/MyFileIniExecDir.txt --------------------\n"
-cat /iExec/MyFileIniExecDir.txt
-touch seeYouInMyZip.txt
+echo "-------------cat /app/MyFileIniExecDir.txt --------------------\n"
+cat /app/MyFileIniExecDir.txt
+
+echo "------------- launch scriptAtRoot.sh --------------------\n"
+bash scriptAtRoot.sh
+echo "-------------generate result --------------------\n"
+touch /iexec/seeYouInMyZip.txt
+touch /DoNOTseeYouInMyZip.txt
---
+++ apps/scriptAtRoot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "-------------This is my scriptAtRoot.sh in docker--------------------\n"
+echo "-------------cat /fileAtRoot.txt --------------------\n"
+cat /fileAtRoot.txt
