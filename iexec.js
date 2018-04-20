1b793da8af3ee5f605eadd7b972118184e5c54ac
victor <victor@vbonhomme.fr>
2018-04-20 23:58:15 +0200

    fix

--- iexec.js
+++ iexec.js
@@ -1,11 +1,11 @@
 module.exports = {
-    name: 'Ffmpeg',
-    app: {
-      type: 'DOCKER',
-      envvars: 'XWDOCKERIMAGE=jrottenberg/ffmpeg:scratch',
-    },
-    work: {
-      cmdline:`-i /iexec/small.mp4 /iexec/small.avi`,
-      dirinuri:'http://techslides.com/demos/sample-videos/small.mp4',
-    }
+  name: 'Ffmpeg',
+  app: {
+    type: 'DOCKER',
+    envvars: 'XWDOCKERIMAGE=jrottenberg/ffmpeg:scratch',
+  },
+  work: {
+    cmdline: '-i /iexec/small.mp4 /iexec/small.avi',
+    dirinuri: 'http://techslides.com/demos/sample-videos/small.mp4',
+  },
 };
