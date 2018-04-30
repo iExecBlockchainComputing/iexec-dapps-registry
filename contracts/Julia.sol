afb0e2ad745a99bdb49337888dcae32ed854d2cd
Ugo Plouviez <up@iex.ec>
2018-04-30 12:16:54 +0200

    Change logo

--- contracts/Julia.sol
+++ contracts/Julia.sol
@@ -3,7 +3,7 @@ import "iexec-oracle-contract/contracts/IexecOracleAPI.sol";
 contract Julia is IexecOracleAPI{
 
     uint public constant DAPP_PRICE = 0;
-    string public constant DAPP_NAME = "init";
+    string public constant DAPP_NAME = "Julia";
     
     function Julia (address _iexecOracleAddress) IexecOracleAPI(_iexecOracleAddress,DAPP_PRICE,DAPP_NAME){
 
