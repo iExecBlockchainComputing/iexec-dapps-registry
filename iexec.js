32a2788f53210661b1a282bf7e40cf5d0a23f1be
fbranciard <francois.branciard@iex.ec>
2018-05-02 17:37:47 +0200

    Update iexec.js

--- iexec.js
+++ iexec.js
@@ -5,7 +5,7 @@ module.exports = {
     envvars: 'XWDOCKERIMAGE=iexechub/r-clifford-attractors',
   },
   work: {
-      cmdline: 'Rscript CliffordAttractors.R -1.24458046630025 -1.25191834103316 -1.81590817030519 -1.90866735205054',
+      cmdline: 'Rscript /iexec/CliffordAttractors.R -1.24458046630025 -1.25191834103316 -1.81590817030519 -1.90866735205054',
       dirinuri: 'https://github.com/iExecBlockchainComputing/iexec-dapp-samples/raw/r-clifford-attractors/apps/CliffordAttractors.R',
   }
 };
